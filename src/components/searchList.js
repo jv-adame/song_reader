28c9f41a2e76776e3f87e8db2869f40c385498d5
unknown <johnvincent.adame@gmail.com>
2017-11-28 11:27:36 -0500

    starting widget integration

--- src/components/searchList.js
+++ src/components/searchList.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 
 //Spotify API implementation
-
+//https://developer.spotify.com/technologies/widgets/spotify-play-button/
 //Song playing will have onClick={()=>this.props.setTone(arr[i].index)}
 class searchList extends Component{
     render(){
@@ -29,6 +29,12 @@ class searchList extends Component{
                     </div>
                     <div className="flexColumn">
                         <button onClick={()=>this.props.inputTone(this.props.searchResults[i].name, this.props.searchResults[i].artists)}>submit</button>
+                        {/* <iframe src="https://open.spotify.com/embed?uri=spotify:track:5JunxkcjfCYcY7xJ29tLai"
+        frameborder="0" allowtransparency="true"></iframe> */}
+
+                        {/* <iframe src={"https://open.spotify.com/embed?uri=spotify:track:" + this.props.searchResults[i].uri}
+        frameborder="0" allowtransparency="true"></iframe> */}
+
                     </div>
                 </div>
             </div>
