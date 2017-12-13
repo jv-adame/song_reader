8aaefa807c700be43176801df5655e0166bbbbe6
spicy182 <johnvincent.adame@gmail.com>
2017-12-12 21:52:45 -0500

    adding to exception handler

--- src/exceptions/exceptions.js
+++ src/exceptions/exceptions.js
@@ -1,9 +1,12 @@
 //First .replace():  Edge case for Helena (So Long & Good Night), categoried simply as Helena at Genius.com
-
+//Second .replace(): Edge case for Andromeda - Gorillaz when titled Andromeda (DRAM Special)
+//Third .replace():  Edge case for Waka Waka - Shakira with subtitle (The Official 2010 FIFA World Cup (TM) Song)
 module.exports =  function refine(title){
     let newCredit = title
-        .replace(/\(So.*\)/g, "");
-
+        .replace(/\(So.*\)/g, "")
+        .replace(/\(DRAM.*\)/g, "")
+        .replace(/\(The Official.*\)/g, "");
+        
     return newCredit;    
 }
     
