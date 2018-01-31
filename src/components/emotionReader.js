b82e8072b5301f6b61a8fbf9ce42151956e0c801
John Vincent Adame <johnvincent.adame@gmail.com>
2018-01-30 22:32:11 -0500

    formatted error messages properly

--- src/components/emotionReader.js
+++ src/components/emotionReader.js
@@ -2,15 +2,15 @@ import React, {Component} from 'react';
 
 class EmotionReader extends Component{
     render(){
+        //Only for use in error messages
+            let playingSong = this.props.song;
+            let playingArtist = this.props.artist[0];
+            let p = Array.from(this.props.percentage);
+            let instrumental = ((p[0] === p[1]) && p[0] === 33);
+            let cantFind = ((p[0] === p[1]) && p[0] === 66);
+        //error message end
         let energy = Math.ceil(this.props.energy * 100);
         let tempo = Math.ceil(this.props.tempo);
-        //Only for use in error messages
-        let playingSong = this.props.song;
-        let playingArtist = this.props.artist[0];
-        let p = Array.from(this.props.percentage);
-        //error message end
-        let instrumental = ((p[0] === p[1]) && p[0] === 33);
-        let cantFind = ((p[0] === p[1]) && p[0] === 66);
         let playing = (this.props.song) ? "flex" : "none";
         let emotionJSX = this.props.emotion.map((thisArray, index)=>{
             let emotion;
@@ -40,7 +40,7 @@ class EmotionReader extends Component{
         if (instrumental)
         {
           return(
-            <div className="errorMessage">
+            <div className="errorMessage readContainer">
               <h2>The song "{playingSong} by {playingArtist}" is an instrumental</h2>
             </div>
           )
@@ -48,11 +48,10 @@ class EmotionReader extends Component{
         else if (cantFind)
         {
           return(
-            <div className="errorMessage">
-              <h2>The song "{playingSong} by {playingArtist}"" cannot be found</h2>
+            <div className="errorMessage readContainer">
+              <h2>The song "{playingSong} by {playingArtist}" cannot be found</h2>
             </div>
           )
-
         }
         else{
           return(
