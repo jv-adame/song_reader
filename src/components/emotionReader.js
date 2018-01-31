c6416e69a932262bd23249528cd6cfe6b8bad5b2
John Vincent Adame <johnvincent.adame@gmail.com>
2018-01-30 22:05:02 -0500

    added error messages

--- src/components/emotionReader.js
+++ src/components/emotionReader.js
@@ -4,6 +4,10 @@ class EmotionReader extends Component{
     render(){
         let energy = Math.ceil(this.props.energy * 100);
         let tempo = Math.ceil(this.props.tempo);
+        //Use this value for error messages
+        let p = Array.from(this.props.percentage);
+        let instrumental = ((p[0] === p[1]) && p[0] === 33);
+        let cantFind = ((p[0] === p[1]) && p[0] === 66);
         let playing = (this.props.song) ? "flex" : "none";
         let emotionJSX = this.props.emotion.map((thisArray, index)=>{
             let emotion;
@@ -20,7 +24,8 @@ class EmotionReader extends Component{
 
             return(
               <div className={emotion}>
-                <div >
+
+                <div>
                   {emotion}:
                 </div>
                 <div>
@@ -29,7 +34,25 @@ class EmotionReader extends Component{
               </div>      
             )
           });
-        return(
+        if (instrumental)
+        {
+          return(
+            <div className="errorMessage">
+              <h2>The song being analyzed is an instrumental</h2>
+            </div>
+          )
+        }
+        else if (cantFind)
+        {
+          return(
+            <div className="errorMessage">
+              <h2>The analyzed song cannot be found</h2>
+            </div>
+          )
+
+        }
+        else{
+          return(
             <div>
               <div className="flexContainer readContainer" style={{display:playing}}>
                   {emotionJSX}
@@ -52,6 +75,8 @@ class EmotionReader extends Component{
               </div>
             </div>
         )
+        }
+
     }
 }
 
